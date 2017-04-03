0d329777680e75e7088bfcea9c3d877b32799887
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-04-03 14:13:19 +0200

    copy JS frontend from indoqa-react-redux

--- createIndoqaBootArchetype.sh
+++ createIndoqaBootArchetype.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+
+# sync frontend with indoqa-react-redux
+rsync -a -v --delete \
+  --exclude "node_modules" \
+  --exclude ".gitignore" \
+  --exclude "pom.xml" \
+  --exclude "dist-zip.xml" \
+  ../indoqa-react-redux/ "./indoqa-quickstart-boot/quickstart-boot-frontend"
+
+# create the archetype project from the project
 cd ./indoqa-quickstart-boot
-mvn clean archetype:create-from-project -Darchetype.filteredExtensions=java
-cd ./target/generated-sources/archetype/
-mvn install
-cd ../../../../
-node postProcessIndoqaBootArchetypePom.js
+mvn clean archetype:create-from-project -Darchetype.filteredExtensions=java -Drat.skip=true
+
+# post process the created archetype (adapt some names, etc.)
+cd ..
+node postProcessIndoqaQuickstartBoot.js
+
+# locally install the archetype
+cd ./indoqa-quickstart-boot/target/generated-sources/archetype/
+mvn install -Drat.skip=true
+cd ../../../../../
