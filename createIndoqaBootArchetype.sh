f9573d6975b49a9ca00310b05196c81da3c4ef56
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-04-03 18:44:47 +0200

    post process package.json

--- createIndoqaBootArchetype.sh
+++ createIndoqaBootArchetype.sh
@@ -6,11 +6,18 @@ rsync -a -v --delete \
   --exclude ".gitignore" \
   --exclude "pom.xml" \
   --exclude "dist-zip.xml" \
+  --exclude "src/doc" \
+  --exclude "docs" \
+  --exclude "target" \
+  --exclude "indoqa-webpack-docs.js" \
+  --exclude ".git" \
   ../indoqa-react-redux/ "./indoqa-quickstart-boot/quickstart-boot-frontend"
 
 # create the archetype project from the project
 cd ./indoqa-quickstart-boot
-mvn clean archetype:create-from-project -Darchetype.filteredExtensions=java -Drat.skip=true
+mvn clean archetype:create-from-project \
+  -Darchetype.filteredExtensions=json \
+  -Drat.skip=true
 
 # post process the created archetype (adapt some names, etc.)
 cd ..
--- postProcessIndoqaQuickstartBoot.js
+++ postProcessIndoqaQuickstartBoot.js
@@ -36,5 +36,28 @@ const removeUnnecessaryFiles = () => {
   console.log(`Removed ${projectFile}`)
 }
 
+const modifyPackageJson = () => {
+  const packageJsonPath = './indoqa-quickstart-boot/target/generated-sources/archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/package.json'
+  const versionReplacement = `"version": "0.0.0",
+  "private": "true",`
+
+  const data = fs.readFileSync(packageJsonPath, 'utf8')
+  const result = data
+    .replace(/Apache-2\.0/g, 'UNLICENSED')
+    .replace(/"version":.".\..\..",/g, versionReplacement)
+    .replace(/indoqa-react-redux-archetype/g, 'quickstart-indoqa-boot-frontend')
+    .replace(/Indoqa React\/Redux Archetype/g, 'Quickstart Indoqa Boot: Frontend')
+    .replace(/git@github\.com:Indoqa\/indoqa-react-redux.git/g, 'git@github.com:Organization/project-name')
+    .replace(/    "docs": "indoqa-webpack \.\/indoqa-webpack-docs\.js",\n/g,'')
+
+
+  fs.writeFileSync(packageJsonPath, result, 'utf8', (err) => {
+     if (err) return console.log(err)
+  })
+
+  console.log(`Modified ${packageJsonPath}`)
+}
+
 modifyPom()
+modifyPackageJson()
 removeUnnecessaryFiles()
