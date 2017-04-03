e3900a0105157dcc9a8a965319ba07ffb35cd8f7
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-04-03 12:15:00 +0200

    remove JS frontend

--- indoqa-quickstart-boot/quickstart-boot-frontend/indoqa-webpack-options.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  devPort: 3333,
-  hotReloadPort: 13333,
-  outputPublicPath: '/assets/',
-  publicPath: '/'
-}
-
--- indoqa-quickstart-boot/quickstart-boot-frontend/src/main/reducers.js
+++
@@ -1,7 +0,0 @@
-import timeReducers from './time/reducers'
-import todoReducers from './todos/reducers'
-
-export default {
-  ...timeReducers,
-  ...todoReducers
-}
