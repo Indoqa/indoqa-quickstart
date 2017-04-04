72b4a21111effd0118265d0e204b0e4fd11ec12e
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-04-04 08:46:43 +0200

    formatting

--- postProcessIndoqaQuickstartBoot.js
+++ postProcessIndoqaQuickstartBoot.js
@@ -5,19 +5,19 @@ const fs = require('fs')
 const modifyPom = () => {
   const pomPath = './indoqa-quickstart-boot/target/generated-sources/archetype/pom.xml'
   const replacement = `<parent>
-      <groupId>com.indoqa</groupId>
-      <artifactId>indoqa-oss-parent</artifactId>
-      <version>4</version>
-    </parent>
+    <groupId>com.indoqa</groupId>
+    <artifactId>indoqa-oss-parent</artifactId>
+    <version>4</version>
+  </parent>
 
-    <name>quickstart-boot-archetype</name>
+  <name>quickstart-boot-archetype</name>
 
-    <scm>
-      <connection>scm:git:git@github.com:Indoqa/indoqa-quickstart.git</connection>
-      <developerConnection>scm:git:git@github.com:Indoqa/indoqa-quickstart.git</developerConnection>
-      <url>https://github.com/Indoqa/indoqa-quickstart</url>
-      <tag>HEAD</tag>
-    </scm>
+  <scm>
+    <connection>scm:git:git@github.com:Indoqa/indoqa-quickstart.git</connection>
+    <developerConnection>scm:git:git@github.com:Indoqa/indoqa-quickstart.git</developerConnection>
+    <url>https://github.com/Indoqa/indoqa-quickstart</url>
+    <tag>HEAD</tag>
+  </scm>
   `
 
   const data = fs.readFileSync(pomPath, 'utf8')
