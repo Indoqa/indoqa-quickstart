30d1e12e1c1903287e8cc42754af469b7912ab81
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-03-26 19:00:51 +0100

    rewrite package.json

--- postProcessIndoqaQuickstartBoot.js
+++ postProcessIndoqaQuickstartBoot.js
@@ -1,62 +1,52 @@
 #! /usr/bin/env node
 const fs = require('fs')
 
-const modifyPom = () => {
-  const pomPath = './indoqa-quickstart-boot/target/generated-sources/archetype/pom.xml'
-  const replacement = `<parent>
-    <groupId>com.indoqa</groupId>
-    <artifactId>indoqa-oss-parent</artifactId>
-    <version>5</version>
-  </parent>
+const allowedDevDependencies = [
+  "react-scripts", 
+  "redux-devtools-extension", 
+  "rimraf", 
+  "source-map-explorer", 
+  "tslint", 
+  "typescript"
+]
 
-  <name>quickstart-boot-archetype</name>
+const modifyRootPackageJson = () => {
+  const baseFilePath = './indoqa-quickstart-boot/target/generated-sources/archetype/src/main/resources/archetype-resources/package.json'
+  const baseFileData = fs.readFileSync(baseFilePath, 'utf8')
 
-  <scm>
-    <connection>scm:git:git@github.com:Indoqa/indoqa-quickstart-boot_release.git</connection>
-    <developerConnection>scm:git:git@github.com:Indoqa/indoqa-quickstart-boot_release.git</developerConnection>
-    <url>https://github.com/Indoqa/indoqa-quickstart-boot_release</url>
-    <tag>HEAD</tag>
-  </scm>
-  `
-
-  const data = fs.readFileSync(pomPath, 'utf8')
-  const result = data.replace(/\<name\>quickstart-boot-archetype\<\/name\>/g, replacement)
-  fs.writeFileSync(pomPath, result, 'utf8', (err) => {
-     if (err) return console.log(err)
+  const includeFilePath = '../indoqa-react/package.json'
+  const packageJson = JSON.parse(fs.readFileSync(includeFilePath, 'utf8'));
+  packageJson.devDependencies["react-scripts"] = "^2.1.8"
+  const resultDevDependencies = {}
+  Object.keys(packageJson.devDependencies).sort().forEach(key => {
+    if (key.startsWith('@types') || allowedDevDependencies.includes(key)) {
+      resultDevDependencies[key] = packageJson.devDependencies[key]
+    }
   })
-
-  console.log(`Modified ${pomPath}`)
+  let devDependencies = `"devDependencies": ${JSON.stringify(resultDevDependencies, null, 4)}`
+  devDependencies = devDependencies.replace(/}/g, '  }')
+  const result = baseFileData.replace(/"devDependencies":(.|\n)*?}/g, devDependencies)
+  fs.writeFileSync(baseFilePath, result, 'utf8', (err) => {
+    if (err) return console.log(err)
+ })
+ 
+ console.log(`Modified ${baseFilePath}`)
 }
 
-const removeUnnecessaryFiles = () => {
-  const projectFile = './indoqa-quickstart-boot/target/generated-sources/archetype/src/main/resources/archetype-resources/.project'
-  if (fs.existsSync(projectFile)) {
-    fs.unlinkSync(projectFile)
-  }
+const modifyFrontendPackageJson = () => {
+  const baseFilePath = './indoqa-quickstart-boot/target/generated-sources/archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/package.json'
+  const baseFileData = fs.readFileSync(baseFilePath, 'utf8')
 
-  console.log(`Removed ${projectFile}`)
-}
-
-const modifyPackageJson = () => {
-  const packageJsonPath = './indoqa-quickstart-boot/target/generated-sources/archetype/src/main/resources/archetype-resources/__rootArtifactId__-frontend/package.json'
-  const versionReplacement = `"version": "0.0.0",
-  "private": "true",`
-
-  const data = fs.readFileSync(packageJsonPath, 'utf8')
-  const result = data
-    .replace(/Apache-2\.0/g, 'UNLICENSED')
-    .replace(/"version":.".\..\..",/g, versionReplacement)
-    .replace(/indoqa-react-redux-ts/g, 'quickstart-indoqa-boot-frontend')
-    .replace(/Indoqa React\/Redux Typescript Archetype/g, 'Quickstart Indoqa Boot: Frontend')
-    .replace(/git@github\.com:Indoqa\/indoqa-react-redux.git/g, 'git@github.com:Organization/project-name')
-    .replace(/    "docs": "indoqa-webpack \.\/indoqa-webpack-docs\.js",\n/g,'')
-
-
-  fs.writeFileSync(packageJsonPath, result, 'utf8', (err) => {
-     if (err) return console.log(err)
+  const includeFilePath = '../indoqa-react/packages/react-starter/package.json'
+  const packageJson = JSON.parse(fs.readFileSync(includeFilePath, 'utf8'));
+  let dependencies = `"dependencies": ${JSON.stringify(packageJson.dependencies, null, 4)}`
+  dependencies = dependencies.replace(/}/g, '  }')
+  const result = baseFileData.replace(/"dependencies":(.|\n)*?}/g, dependencies)
+  fs.writeFileSync(baseFilePath, result, 'utf8', (err) => {
+    if (err) return console.log(err)
   })
-
-  console.log(`Modified ${packageJsonPath}`)
+  
+  console.log(`Modified ${baseFilePath}`)
 }
 
 const createGitignore = () => {
@@ -81,4 +71,6 @@ target
   console.log(`Created ${gitIgnorePath}`)
 }
 
+modifyRootPackageJson()
+modifyFrontendPackageJson()
 createGitignore()
